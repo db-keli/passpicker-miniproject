e80efc29ce8410cac738396698093b77d1580376
kekeli-the-light <kekelidompeh@gmail.com>
2023-03-28 15:29:47 +0000

    Looping

--- PassPicker/main.py
+++ PassPicker/main.py
@@ -1,8 +1,13 @@
 from password import *
 
 Welcome()
-nounread()
-adj_read()
-random_num()
-charread()
-print ("Password is: " + nounread() + adj_read() + charread() + random_num())
+while True:   
+    nounread()
+    adj_read()
+    random_num()
+    charread()
+    print ("Password is: " + nounread() + adj_read() + charread() + random_num())
+    response = input("\nWant some more? Type y or n: \n")
+    print("\n")
+    if response == "n":
+        break
